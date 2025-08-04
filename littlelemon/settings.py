749b277437f7c16075577e7a25d8ea41e4cafab1
Yoschi95 <joscha.hartmann@web.de>
2025-08-04 19:41:42 +0200

    Add app restaurant

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
