b8ba211c8ef0ff1212518dfa7dc8f5fe5bdf31a2
Yoschi95 <joscha.hartmann@web.de>
2025-08-05 21:37:26 +0200

    Update database settings and add models for Menu and Booking

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -77,9 +77,16 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        'USER' : 'root',
+        'PASSWORD' : 'root',
+        'OPTIONS' : {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Menu, Booking
 
 # Register your models here.
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Title', 'Price', 'Inventory')  # Replace with your actual field names
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Name', 'BookingDate', 'No_of_guests')  # Replace with your actual field names
+
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(Booking, BookingAdmin)
